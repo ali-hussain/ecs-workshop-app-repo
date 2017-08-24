16d067ddd2ba6d095094aa8726a4780a4e4219c1
Ali Hussain <ali@flux7.com>
2017-08-24 00:04:41 -0500

    Make S3 sync conditional on bucket defined

--- nginx_wrapper.sh
+++ nginx_wrapper.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-aws s3 sync $(RESOURCE_BUCKET)${RESOURCE_BUCKET_PATH}/ /usr/share/nginx/html/
+if [ -z ${RESOURCE_BUCKET} ]; then
+  echo get images from S3
+  aws s3 sync $(RESOURCE_BUCKET)${RESOURCE_BUCKET_PATH}/ /usr/share/nginx/html/
+fi
 envsubst < /etc/nginx/conf.d/mysite.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
