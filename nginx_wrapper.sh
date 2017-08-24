d2d3a1c8f8c12807b489e7c49c813b30f24e3551
Ali Hussain <ali@flux7.com>
2017-08-24 00:15:05 -0500

    Announcing the use of script

--- nginx_wrapper.sh
+++ nginx_wrapper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo deploy nginx using nginx_wrapper.sh
 if [ -z ${RESOURCE_BUCKET} ]; then
   echo get images from S3
   aws s3 sync $(RESOURCE_BUCKET)${RESOURCE_BUCKET_PATH}/ /usr/share/nginx/html/
